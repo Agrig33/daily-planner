6962c952b9a7f81a96b6ae411fcad8613f176876
{airin grigorian} <agrigorian33@gmail.com>
2024-01-30 12:38:21 -0800

    removed extra parenthesis from line 19 on js, was causing error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-  $(document).ready(function() {
+$(document).ready(function() {
   
 // event listener
   $('.saveBtn').on('click', function() {
@@ -7,7 +7,7 @@
     var time = $(this).parent().attr('id');
 
 //save to localStorage
-  localStorage.setItem(time,value);
+  localStorage.setItem(time, value);
 });
 
 //current number of hours
@@ -16,7 +16,7 @@
 
 //loop over time blocks
 $('.time-block').each(function() {
-    var blockHour = parseInt($(this)).attr('id').split('-')[1]);
+    var blockHour = parseInt($(this).attr('id').split('-')[1]);
 
 //check if time has passed from current hour
 if (blockHour < currentHour) {
@@ -25,28 +25,28 @@ if (blockHour < currentHour) {
 else if (blockHour === currentHour) {
   $(this).removeClass('past');
   $(this).addClass('present');
-} else {
+} 
+else {
   $(this).removeClass('past');
   $(this).removeClass('present');
   $(this).addClass('future');
 }
-
 });
-
+}
 hourUpdater();
 
 var interval = setInterval(hourUpdater, 15000);
 
 // load any saved data from localStorage
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
-  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  $('#currentDay').text(moment().format('dddd, MMMM Do'));
 });
