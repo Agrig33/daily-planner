4ec7d50736d6aaed1777a1c9bb4b1013aee02aca
{airin grigorian} <agrigorian33@gmail.com>
2024-01-30 00:47:27 -0800

    changed some class id's on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+  $(document).ready(function () {
+  
+// event listener
+  $('.saveBtn').on('click', function() {
+
+    var value = $(this).siblings('.description').val();
+    var time =(this).parent().attr('id');
+
+//save to localStorage
+  localStorage.setItem(time,value);
+});
+
+//current number of hours
+  function hourUpdater() {
+    var currentHour = dayjs().hour();
+
+//
+
+
+}
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
